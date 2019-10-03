087c15bc32174275127b0c21a45daf01e06864d1
pietro1704 <pietropugliesi@icloud.com>
2019-10-03 15:20:57 -0300

    lógica escrita

--- 202595.pl
+++ 202595.pl
@@ -1,3 +1,25 @@
 % Luma Gabino Vasconcelos RA: 202495
 % Pietro Ruy Pugliesi RA: 185921
 
+
+%rodar como 
+%swipl -q -f seu-prog.pr -t topo  < arqtestes.in
+%ler os dados
+
+%pegar informacoes das figuras(Circulo: posicao do centro e raio,
+% quadrado: centro e lados -> vertices)
+
+%fazer os algoritmo tudo
+
+%intersecao quadrado-quadrado
+
+%intersecao circulo-circulo
+
+%intersecao quadrado-circulo
+
+%talvez separar pra nao ler a mesma intercessao 2 vezes
+
+%IMPRESSAO:
+%1. quantidade de pares com imtercessao
+%2. nomes dos pares de figuras
+
